fc6e97d5b36686399b5aa04039cdc856c4e51e49
ayushvats1227 <ayush2089.be21@chitkara.edu.in>
2024-04-23 09:53:06 +0530

    Add: Datamodels added

---
+++ ElderTales/ElderTales/DataModel.swift
@@ -0,0 +1,51 @@
+//
+//  File.swift
+//  ElderTales
+//
+//  Created by Student on 22/04/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct User {
+    var name: String
+    var email: String
+    var isElderly: Bool
+    var password: String
+    var image: UIImage
+    var likedPosts: [Post]
+    var savedPosts: [Post]
+    var following: [User]
+    var interests: [Category]
+}
+
+struct Post {
+    var postedBy: User
+    var postedOn: Date
+    var length: Int
+    var title: String
+    var cover: UIImage
+    var likes: Int
+    var comments: [Comment]
+    var shares: Int
+    var suitableCategories: [Category]
+}
+
+struct Live{
+    var postedBy: User
+    var postedOn: Date
+    var beginsOn: Date
+    var interested: [User]
+}
+
+struct Comment {
+    var postedBy: User
+    var postedOn: Date
+    var body: String
+}
+
+struct Category{
+    var title: String
+    var image: UIImage
+}
