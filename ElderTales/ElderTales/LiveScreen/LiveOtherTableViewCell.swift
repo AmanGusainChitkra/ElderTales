53a30f8ab1b93359369f0c7f176b721d6fc3fba3
AmanGusainChitkra <aman4814.be21@chitkara.edu.in>
2024-04-30 11:02:00 +0530

    Working: Added files inside Live folder

---
+++ ElderTales/ElderTales/LiveScreen/LiveOtherTableViewCell.swift
@@ -0,0 +1,49 @@
+//
+//  LiveOtherTableViewCell.swift
+//  ElderTales
+//
+//  Created by student on 25/04/24.
+//
+
+import UIKit
+
+class LiveOtherTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var profilePhotoUIImage: UIImageView!
+    
+    @IBOutlet weak var storyTitle: UILabel!
+    
+    @IBOutlet weak var username: UILabel!
+    
+    @IBOutlet weak var upcomming: UILabel!
+    
+    @IBOutlet weak var intrested: UILabel!
+    
+    @IBOutlet weak var dateOfLive: UILabel!
+    
+    @IBOutlet weak var timeOfLive: UILabel!
+    
+    
+    
+    @IBOutlet weak var thumbnailUIImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+   profilePhotoUIImage.layer.cornerRadius = 20
+    }
+    
+    
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        super.setSelected(false, animated: animated)
+        // Configure the view for the selected state
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 20, left: 10, bottom: 20, right: 10))
+    }
+
+}
---
+++ ElderTales/ElderTales/LiveScreen/LiveViewController.swift
@@ -0,0 +1,56 @@
+//
+//  LiveViewController.swift
+//  ElderTales
+//
+//  Created by student on 25/04/24.
+//
+
+import UIKit
+import SwiftUI
+
+class LiveViewController: UIViewController, UITableViewDataSource{
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return lives.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = liveTableView.dequeueReusableCell(withIdentifier: "liveOther", for: indexPath) as! LiveOtherTableViewCell
+
+        let live = lives[indexPath.row]
+        
+        // Configure the cell with data from the post
+        cell.profilePhotoUIImage.image = UIImage(named: "otherPhoto")
+        cell.username.text = live.postedBy.name
+        cell.storyTitle.text = live.title
+        cell.thumbnailUIImage.image = UIImage(named: "otherPhoto")
+        
+        
+        
+        return cell
+        
+    }
+    
+    @IBOutlet weak var liveTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        generateDummyData()
+        super.viewDidLoad()
+       liveTableView.dataSource = self
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
